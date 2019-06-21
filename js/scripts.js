be559171b3e780b668acdb0f551f13a92d7a1526
Lake Bramlett <lake.bramlett@gmail.com>
2019-06-21 10:23:59 -0700

    add Pizza property value based on user input and data

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 ////business logic////
-function Order(orderName, orderItems, deliverTo, orderTotal) {
-  this.orderName = orderName;
+function Order(orderName, deliverTo) {
+  this.orderName = '';
   this.orderItems = [];
   this.deliverTo = deliverTo;
-  this.orderTotal = orderTotal;
+  this.orderTotal = 0;
 }
 
 function Pizza(size, toppings) {
@@ -11,21 +11,34 @@ function Pizza(size, toppings) {
   this.toppings = toppings;
 }
 
-Pizza.prototype.addTotal = function () {
-  return pizzaOrder.size + pizzaOrder.toppings.length;
+Pizza.prototype.pizzaTotal = function () {
+  return pizza.size + pizza.toppings.length;
 };
 
 
 ////user interface logic////
 
-var order = new Order;
+var order = new Order
+
+function toppings() {
+  var toppingsArray = [];
+  $(".toppings input:checked").each(function() {
+    toppingsArray.push($(this).val());
+  });
+  return toppingsArray;
+}
+
+function addPizza() {
+  var pizza = new Pizza (parseInt($('.size input:checked').val()), toppings());
+  order.orderItems.push(pizza);
+  order.orderTotal += (pizza.size + pizza.toppings.length);
+};
 
 $(document).ready(function() {
  console.log('jquery enabled');
-  $('button add-pizza').click(function() {
+  $('button.add-pizza').click(function() {
     console.log('button clicked');
-    var pizzaOrder = new Pizza ('lake', 12, ['Pineapple', 'Canadian Bacon', 'Onion']);
-    order.orderItems.push(pizzaOrder);
+     addPizza();
   });
 
 
