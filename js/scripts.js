23dff71df91843f91d922c698893fed857a7e6b8
Lake Bramlett <lake.bramlett@gmail.com>
2019-06-21 08:54:38 -0700

    create Pizza constructor and initial pizzaOrder instance of Pizza object

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
 ////business logic////
+function Pizza(orderName, size, toppings) {
+  this.orderName = orderName;
+  this.size = size;
+  this.toppings = toppings;
+}
 
+var pizzaOrder = new Pizza ('lake', 12, ['Pineapple', 'Canadian Bacon', 'Onion'])
 ////user interface logic////
 
 
 
 $(document).ready(function() {
  console.log('jquery enabled');
+  $('button').click(function() {
+    console.log('button clicked');
+  });
 
 
 });//jquery
